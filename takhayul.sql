7096325743133ff15abdafa38e68b8b0730eff14
rayaadhary <adharyr9@gmail.com>
2023-01-29 17:12:24 +0700

    db

--- takhayul.sql
+++ takhayul.sql
@@ -1,77 +1,139 @@
-/*
-SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 10.4.22-MariaDB : Database - takhayul
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 29, 2023 at 11:11 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`takhayul` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `takhayul`;
+--
+-- Database: `takhayul`
+--
 
-/*Table structure for table `artikel` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `artikel`;
+--
+-- Table structure for table `artikel`
+--
 
 CREATE TABLE `artikel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `judul` text NOT NULL,
-  `deskripsi` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `artikel` */
+--
+-- Dumping data for table `artikel`
+--
 
-insert  into `artikel`(`id`,`judul`,`deskripsi`) values 
-(1,'Asal Usul Takhayul Kucing Hitam yang Dianggap Bawa Sial dan Faktanya','Asal usul takhayul tentang kucing hitam dikaitkan dengan kemalangan dan kemalangan datang dari zaman dahulu. Dalam mitologi Kristen, kucing hitam dianggap sebagai makhluk yang dikendalikan oleh iblis dan sering dikaitkan dengan kejadian buruk. Dalam mitologi Inggris, kucing hitam dianggap sebagai pembawa keberuntungan buruk. Namun, dalam budaya lain seperti Cina dan Jepang, kucing hitam dianggap sebagai simbol keberuntungan.Beberapa teori menyatakan bahwa keyakinan ini berasal dari persekutuan kucing dengan dunia kepercayaan pagan, di mana kucing dianggap sebagai makhluk yang dapat menghubungkan dunia manusia dengan dunia roh. Kucing juga dianggap sebagai pelindung rumah dari roh jahat dan kemalangan. Namun, faktanya, kucing hitam sama seperti kucing lainnya dan tidak lebih atau kurang berbahaya atau beruntung. Beberapa orang mungkin merasa tidak nyaman dengan kucing hitam karena mitos atau karena warna kulit yang unik, tetapi ini tidak ada hubungannya dengan karakter atau perilaku kucing itu sendiri.');
+INSERT INTO `artikel` (`id`, `judul`, `deskripsi`) VALUES
+(1, 'Kucing hitam membawa sial?', 'Asal usul takhayul tentang kucing hitam dikaitkan dengan kemalangan dan kemalangan datang dari zaman dahulu. Dalam mitologi Kristen, kucing hitam dianggap sebagai makhluk yang dikendalikan oleh iblis dan sering dikaitkan dengan kejadian buruk. Dalam mitologi Inggris, kucing hitam dianggap sebagai pembawa keberuntungan buruk. Namun, dalam budaya lain seperti Cina dan Jepang, kucing hitam dianggap sebagai simbol keberuntungan.Beberapa teori menyatakan bahwa keyakinan ini berasal dari persekutuan kucing dengan dunia kepercayaan pagan, di mana kucing dianggap sebagai makhluk yang dapat menghubungkan dunia manusia dengan dunia roh. Kucing juga dianggap sebagai pelindung rumah dari roh jahat dan kemalangan. Namun, faktanya, kucing hitam sama seperti kucing lainnya dan tidak lebih atau kurang berbahaya atau beruntung. Beberapa orang mungkin merasa tidak nyaman dengan kucing hitam karena mitos atau karena warna kulit yang unik, tetapi ini tidak ada hubungannya dengan karakter atau perilaku kucing itu sendiri.'),
+(2, 'Duduk di atas bantal, bisulan?', 'Ada beberapa teori yang menyatakan bahwa duduk di atas bantal dapat menyebabkan bisulan. Namun, tidak ada bukti ilmiah yang kuat yang mendukung afirmasi ini. Faktor lain seperti kondisi kulit dan hygiene dapat lebih mempengaruhi munculnya bisulan daripada duduk di atas bantal. Namun, jika seseorang merasa nyaman dengan duduk di atas bantal atau jika itu membantu dalam mengurangi nyeri pinggul atau punggung, tidak ada alasan untuk menghindarinya.');
 
-/*Table structure for table `jawaban` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `jawaban`;
+--
+-- Table structure for table `jawaban`
+--
 
 CREATE TABLE `jawaban` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `id_pertanyaan` int(11) DEFAULT NULL,
   `deskripsi` text DEFAULT NULL,
-  `jawaban` tinyint(2) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_pertanyaan` (`id_pertanyaan`),
-  CONSTRAINT `jawaban_ibfk_1` FOREIGN KEY (`id_pertanyaan`) REFERENCES `pertanyaan` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+  `jawaban` tinyint(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `jawaban` */
+--
+-- Dumping data for table `jawaban`
+--
 
-insert  into `jawaban`(`id`,`id_pertanyaan`,`deskripsi`,`jawaban`) values 
-(1,1,'Ya',0),
-(2,1,'Tidak',1),
-(3,2,'Ya',0),
-(4,2,'Tidak',1);
+INSERT INTO `jawaban` (`id`, `id_pertanyaan`, `deskripsi`, `jawaban`) VALUES
+(1, 1, 'Ya', 0),
+(2, 1, 'Tidak', 1),
+(3, 2, 'Ya', 0),
+(4, 2, 'Tidak', 1);
 
-/*Table structure for table `pertanyaan` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `pertanyaan`;
+--
+-- Table structure for table `pertanyaan`
+--
 
 CREATE TABLE `pertanyaan` (
   `id` int(11) NOT NULL,
   `deskripsi` text DEFAULT NULL,
-  `skor` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `skor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `pertanyaan` */
+--
+-- Dumping data for table `pertanyaan`
+--
 
-insert  into `pertanyaan`(`id`,`deskripsi`,`skor`) values 
-(1,'Apakah zodiak mempengaruhi kepribadian seseorang?',10),
-(2,'Apakah gempa pertanda dewa marah?',10);
+INSERT INTO `pertanyaan` (`id`, `deskripsi`, `skor`) VALUES
+(1, 'Apakah zodiak mempengaruhi kepribadian seseorang?', 10),
+(2, 'Apakah gempa pertanda dewa marah?', 10);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jawaban`
+--
+ALTER TABLE `jawaban`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pertanyaan` (`id_pertanyaan`);
+
+--
+-- Indexes for table `pertanyaan`
+--
+ALTER TABLE `pertanyaan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `jawaban`
+--
+ALTER TABLE `jawaban`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jawaban`
+--
+ALTER TABLE `jawaban`
+  ADD CONSTRAINT `jawaban_ibfk_1` FOREIGN KEY (`id_pertanyaan`) REFERENCES `pertanyaan` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
