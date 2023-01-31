cf9afa42aa2dda6a53b2354dddb26cb6c7c11528
rayaadhary <adharyr9@gmail.com>
2023-01-31 20:24:31 +0700

    validasi form

--- app/views/post/formTambah.php
+++ app/views/post/formTambah.php
@@ -4,6 +4,8 @@
 
   <div class="course">
     <div class="container">
+
+
       <div class="row">
 
         <!-- Panels -->
@@ -14,17 +16,26 @@
               <div class="form_title">Tambah Artikel</div>
             </div>
             <div class="card-body">
-              <form action="<?= BASEURL ?>/post/tambah" method="post" enctype="multipart/form-data">
-                <div class="form-group">
+              <form class="was-validated" action="<?= BASEURL ?>/post/tambah" method="post" enctype="multipart/form-data">
+                <div class="form-group has-validation">
                   <label for="judul" class="text-dark">Judul Artikel</label>
-                  <input type="text" name="judul" class="form-control" id="judul" aria-describedby="judul" placeholder="isi dengan judul artikel">
+                  <input type="text" name="judul" class="form-control" id="judul" aria-describedby="judul" placeholder="isi dengan judul artikel" required>
+                  <div class="invalid-feedback">
+                    Mohon tuliskan judul
+                  </div>
                 </div>
-                <div class="form-group">
+                <div class="form-group has-validation">
                   <label for="artikel" class="text-dark">Artikel</label>
-                  <textarea class="form-control" name="deskripsi" id="artikel" placeholder="isi dengan artikel" rows="8"></textarea>
+                  <textarea class="form-control" name="deskripsi" id="artikel" placeholder="isi dengan artikel" rows="8" required></textarea>
+                  <div class="invalid-feedback">
+                    Mohon tuliskan artikel
+                  </div>
                 </div>
-                <div class="form-group">
-                  <input type="file" name="gambar" id="">
+                <div class="form-group has-validation">
+                  <input type="file" name="gambar" id="" required>
+                  <div class="invalid-feedback">
+                    Mohon unggah gambar
+                  </div>
                 </div>
                 <div class="text-center">
                   <button type="submit" class="form_button trans_200">Kirim Artikel</button>
